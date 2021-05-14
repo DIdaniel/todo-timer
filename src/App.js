d61db614a46f9fca4165775fce4a9b777496e162
dream_Faunus <cappu159@gmail.com>
2021-05-14 11:44:12 +0900

    install and getting ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from "styled-components";
+import TodoApp from "./components/todo/TodoApp";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #DB524D;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoApp />
+      <GlobalStyle />
+    </>
   );
 }
 
---
+++ src/components/todo/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p>WORKING ON</p>
+      <p></p>
+    </div>
+  );
+};
+
+export default Header;
